e0a781d3a040d877af7d53180e6ed9007e3116f0
wengtianliang <wengtianliang@jkweixin.com>
2018-07-10 21:00:22 +0800

    添加了客户资料，客户详情

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,9 @@ const Oauth = () => import('pages/oauth/oauth')
 const Overview = () => import('pages/overview/overview')
 const Ranking = () => import('pages/ranking/ranking')
 const AI = () => import('pages/ai-analyse/ai-analyse')
+const CustomerList = () => import('pages/customer-list/customer-list')
+const CustomerDetail = () => import('pages/customer-detail/customer-detail')
+const CustomerData = () => import('pages/customer-data/customer-data')
 
 Vue.use(Router)
 
@@ -25,7 +28,34 @@ const route = new Router({
       component: Overview,
       meta: {
         title: '总览'
-      }
+      },
+      children: [
+        {
+          path: 'customer-list',
+          component: CustomerList,
+          meta: {
+            title: '客户列表'
+          },
+          children: [
+            {
+              path: 'customer-detail',
+              component: CustomerDetail,
+              meta: {
+                title: '客户详情'
+              },
+              children: [
+                {
+                  path: 'customer-data',
+                  component: CustomerData,
+                  meta: {
+                    title: '客户资料'
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
     },
     {
       path: '/ranking',
@@ -39,6 +69,20 @@ const route = new Router({
       component: AI,
       meta: {
         title: 'AI分析'
+      }
+    },
+    {
+      path: '/customer-detail',
+      component: CustomerDetail,
+      meta: {
+        title: '客户详情'
+      }
+    },
+    {
+      path: '/customer-data',
+      component: CustomerData,
+      meta: {
+        title: '客户详情'
       }
     }
   ]
